2bed29ade1daefcc5236346cf162133ce9c38845
williamdoyle47 <williamdoyle2000@gmail.com>
2023-02-06 16:02:03 -0600

    faster inference speed update
    load model weights using .pb file
    take advantage of graph mode execution of TF

--- FodApp/src/detection_modules/DetectionModel.py
+++ FodApp/src/detection_modules/DetectionModel.py
@@ -1,17 +1,20 @@
-import numpy as np
 import tensorflow as tf
 import os
 import cv2
 import json
 import random
+import requests
+import uuid
+import numpy as np
 from detection_modules.DetectionLogging import LogDetection
 from PIL import Image
 from object_detection.utils import label_map_util
 from object_detection.utils import visualization_utils as viz_utils
+from detection_modules.coords import *
 from object_detection.builders import model_builder
 from object_detection.utils import config_util
 import requests
-from detection_modules.coords import coords
+from detection_modules.coords import coords, magnet, sweeping, rumble_strips, fod_containers
 
 
 class DetectionModel:
@@ -22,42 +25,36 @@ class DetectionModel:
         self.url = "http://127.0.0.1:8000/add_fod"
         self.saved_model_path = "/Users/williamdoyle/Documents/GitHub/Airport-Runway-FOD/FodApp/src/Tensorflow/workspace/workspace/models/ssd_mobnet640/export/saved_model"
         self.label_map_name = "/Users/williamdoyle/Documents/GitHub/Airport-Runway-FOD/FodApp/src/Tensorflow/workspace/annotations/label_map.pbtxt"
-        self.d2PathCkpt = '/Users/williamdoyle/Documents/GitHub/Airport-Runway-FOD/FodApp/src/Tensorflow/workspace/workspace/models/ssd_mobnet640'
-        self.d2Config = '/Users/williamdoyle/Documents/GitHub/Airport-Runway-FOD/FodApp/src/Tensorflow/workspace/workspace/models/ssd_mobnet640/pipeline.config'
-        self.load_model(self.label_map_name, self.d2PathCkpt, self.d2Config)
+        self.load_model()
 
-    def load_model(self, label_map_name, d2PathCkpt, d2Config):
+    def load_model(self):
         try:
-            # Load pipeline config and build a detection model
-            self.configs = config_util.get_configs_from_pipeline_file(d2Config)
-            self.detection_model = model_builder.build(
-                model_config=self.configs['model'], is_training=False)
-            # Restore checkpoint
-            self.ckpt = tf.compat.v2.train.Checkpoint(
-                model=self.detection_model)
-            # ensure up to date with current checkpoint
-            self.ckpt.restore(os.path.join(
-                d2PathCkpt, 'ckpt-8')).expect_partial()
-            self.category_index = label_map_util.create_category_index_from_labelmap(
-                label_map_name)
-        except:
-            print("Error loading model, check inputs")
+            self.saved_model = tf.saved_model.load(self.saved_model_path)
 
-    def detect_fn(self, input_tensor):  # connects to TF API
-        input_tensor, shapes = self.detection_model.preprocess(input_tensor)
-        prediction_dict = self.detection_model.predict(input_tensor, shapes)
-        detections = self.detection_model.postprocess(prediction_dict, shapes)
+            self.category_index = label_map_util.create_category_index_from_labelmap(
+                self.label_map_name)
+        except:
+            print("Error loading model -- check saved model")
+
+    @tf.function  # graph mode execution decorator -- pre loads model weights leading to faster inference
+    # Connect to tf obj detection api to make detection using our model
+    def detect_fn(self, input_tensor):
+        detections = self.saved_model(input_tensor)
         return detections
 
     def make_detections(self, image_np):
-        input_tensor = tf.convert_to_tensor(
-            np.expand_dims(image_np, 0), dtype=tf.float32)
+
+        # Pre Process Image Frame
+        input_tensor = tf.convert_to_tensor(image_np)
+        input_tensor = input_tensor[tf.newaxis, ...]
+        input_tensor = input_tensor[:, :, :, :3]
+
+        # manipulate prediction dict
         detections = self.detect_fn(input_tensor)
         num_detections = int(detections.pop('num_detections'))
         detections = {key: value[0, :num_detections].numpy()
                       for key, value in detections.items()}
         detections['num_detections'] = num_detections
-        # detection_classes should be ints.
         detections['detection_classes'] = detections['detection_classes'].astype(
             np.int64)
 
@@ -77,57 +74,75 @@ class DetectionModel:
             agnostic_mode=False)
         return image_np
 
-    def bndcoords(self, image_np, detections):
-        im = Image.fromarray(image_np)
-        im_width = im.width
-        im_height = im.height
-        xmin, ymin, xmax, ymax = detections['detection_boxes'][0]
-        (x, x2, y, y2) = (xmin * im_width, xmax *
-                          im_width, ymin * im_height, ymax * im_height)
-        w = x2-x
-        h = y2-y
-        return x, y, w, h
+    # def bndcoords(self, image_np, detections):  # Crop image
+    #     im = Image.fromarray(image_np)
+    #     im_width = im.width
+    #     im_height = im.height
+    #     xmin, ymin, xmax, ymax = detections['detection_boxes'][0]
+    #     (x, x2, y, y2) = (xmin * im_width, xmax *
+    #                       im_width, ymin * im_height, ymax * im_height)
+    #     w = x2-x
+    #     h = y2-y
+    #     return x, y, w, h
 
-    # Log interactions
+    def recommend_action(self, fod_type):
+        if fod_type in magnet:
+            rec_cleanup_method = 'magnet'
+        elif fod_type in sweeping:
+            rec_cleanup_method = 'sweeping'
+        elif fod_type in fod_containers:
+            rec_cleanup_method = 'fod_containers'
+        elif fod_type in rumble_strips:
+            rec_cleanup_method = 'rumble_strips'
+        else:
+            rec_cleanup_method = 'Not sure'
+        return rec_cleanup_method
+
     def logging_detection(self, detections, boundary_boxes):
 
         confidence_score = detections['detection_scores'][0]
+        fod_uuid = uuid.uuid4()
 
-        magnet = ['metal', 'screw']  # metallic objects
-        sweeping = ['pen', 'glove', 'cloth', 'LuggageTag']
-        rumble_strips = ['']
-        fod_containers = ['wood']
+        # store images by uuid
+        try:
+            magnet = ['metal', 'screw']  # metallic objects
+            sweeping = ['pen', 'glove', 'cloth', 'LuggageTag']
+            rumble_strips = ['']
+            fod_containers = ['wood']
 
-        fod_type = self.category_index.get(
-            (detections['detection_classes'][0] + self.label_id_offset))['name']
-        image_path = "/Users/williamdoyle/Documents/GitHub/Airport-Runway-FOD/FodApp/src/data_modules/detectionImages/" + \
-            str(self.pathnumber) + '.jpg'  # generalize
+            fod_type = self.category_index.get(
+                (detections['detection_classes'][0] + self.label_id_offset))['name']  # get deteection class
+            image_path = "/Users/williamdoyle/Documents/GitHub/Airport-Runway-FOD/FodApp/src/data_modules/detectionImages/" + \
+                str(fod_uuid) + '.jpg'
+            cropped = Image.fromarray(boundary_boxes)
+            cropped.save(image_path, 'JPEG')
+        except:
+            print("exception occured in logging detection")
+            return
 
-        cropped = Image.fromarray(boundary_boxes)
-        cropped.save(image_path, 'JPEG')
-        self.pathnumber += 1
-
-        cleaning_methods = [magnet, sweeping,
-                            rumble_strips, fod_containers]
-        rec_cleanup_method = any(
-            fod_type in sublist for sublist in cleaning_methods)
-
+        # Log detected fod to database using post request
+        rec_cleanup_method = self.recommend_action(fod_type)
         cleaned = False
-
         coor = random.choice(coords)
 
+        image_path = "/detectionImages/" + str(fod_uuid) + '.jpg'  # fix this
         image_path = "../../data_modules/detectionImages/" + \
             str(self.pathnumber) + '.jpg'
 
         det_json_obj = {'fod_type': str(fod_type),
+                        'uuid': str(fod_uuid),
                         'coord': str(coor),
                         'confidence_level': float(confidence_score),
                         'image_path': str(image_path),
                         'cleaned': bool(cleaned),
                         'recommended_action': str(rec_cleanup_method)
                         }
-        x = requests.post(self.url, json=det_json_obj)
-        print(x.text)
+
+        try:
+            x = requests.post(self.url, json=det_json_obj)
+            print(x.text)
+        except:
+            pass
 
     def detection_controller(self, image_np):
         detections = self.make_detections(image_np)
@@ -138,19 +153,14 @@ class DetectionModel:
             #cleaned = false
 
             if confidence_score > self.threshold:
-
-                self.logging_detection(detections, boundary_boxes)
-
-            # ensure we are looking at different detecion object -- future update
+                # ensure we are looking at different detecion object -- future update
                 # if so create detection object
                 # call logging function()
-                # recommend action() + send notification
-
-            # dist est. function
+                self.logging_detection(detections, boundary_boxes)
 
             return boundary_boxes
 
-        except IndexError:
+        except IndexError:  # if there is no object in frame to detect
             return boundary_boxes
 
 
